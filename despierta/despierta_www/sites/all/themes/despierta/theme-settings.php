e30f89bfdfbe4e42830eb84a1d1509f763c4a0e0
josemrc <josemrc@gmail.com>
2015-10-20 11:22:38 +0200

    Next commit

---
+++ despierta/despierta_www/sites/all/themes/despierta/theme-settings.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @file
+ * Theme settings file for the despierta theme.
+ */
+
+require_once dirname(__FILE__) . '/template.php';
+
+/**
+ * Implements hook_form_FORM_alter().
+ */
+function despierta_form_system_theme_settings_alter(&$form, $form_state) {
+  // You can use this hook to append your own theme settings to the theme
+  // settings form for your subtheme. You should also take a look at the
+  // 'extensions' concept in the Omega base theme.
+
+dprint_r("HOLA Registro");	
+}
+function despierta_form_user_register_alter(&$form, &$form_state, $form_id) {
+dprint_r("HOLA Registro");
+  $form['account']['name']['#title'] = t('Full name');
+  // perform other changes here
+}
