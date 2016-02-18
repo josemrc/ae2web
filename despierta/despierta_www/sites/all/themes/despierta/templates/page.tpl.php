2f0b734d8390e41429c3e9124db9eb60dd00d5c9
josemrc <josemrc@gmail.com>
2016-02-18 09:06:20 +0100

    Next commit

--- despierta/despierta_www/sites/all/themes/despierta/templates/page.tpl.php
+++ despierta/despierta_www/sites/all/themes/despierta/templates/page.tpl.php
@@ -263,16 +263,6 @@
 <div class="modal fade" tabindex="-1" role="dialog" id="sms_geoloc_nodespierta">
   <div class="modal-dialog">
     <div class="modal-content">
-<!--
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title">Información de geolocalización</h4>
-      </div>
-      <div class="modal-body">
-        <p>Se encuentra geolocalizado pero no existen sedes en su país o región.</p>
-        <p>Seleccione el país y regiones mediante las opciones del panel superior derecho en la web</p>
-       </div>
--->
       <div class="modal-header">
         <h4 class="modal-title text-center">Selecione un país y su región</h4>
       </div>
